ecd8479c09b5794d70e72391749a1026908348bd
Melinda Cardenas <mc6377@nyu.edu>
2021-04-12 00:21:21 -0400

    add routes (temporary)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-import GetUserInfo from "./carbonEstimation";
+import {BrowserRouter,Route, Switch} from 'react-router-dom'
+import Home from './components/Home';
+import FootprintQuiz from "./components/footprint-quiz";
+//import GetUserInfo from "./carbonEstimation";
+import Header from "./components/Header"
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, ThemeProvider, MuiThemeProvider } from "@material-ui/core/styles";
 
+const theme = createMuiTheme({
+  
+  palette:{
+    primary: {
+      main: '#000000',
+      //mainGradient: "linear-gradient(#157A42,#25DB77)"
+      //background: 'linear gradient(#157A42,#25DB77)',
+    },
+    background: {default:'#157A42'}
+    
+  }
+})
+//style={{ background: theme.palette.primary.mainGradient }}
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-      </header> */}
-      <GetUserInfo />
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/quiz">
+            <FootprintQuiz />
+          </Route>
+          <Route path="estimate-quize">
+            {/* {<GetUserInfo />} */}
+          </Route>
+        </Switch>  
+      </BrowserRouter>    
+    </MuiThemeProvider>
   );
 }
 
