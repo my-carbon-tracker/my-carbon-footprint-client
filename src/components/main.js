32cd0d688876d3a344e9c246b65a4d9e3a85afd3
Jaylene Bautista <jaylenebautista@Jaylenes-MacBook-Pro-5.local>
2021-04-18 16:44:47 -0400

    main page updated

--- src/components/main.js
+++ src/components/main.js
@@ -4,48 +4,40 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
-import Image from '../images/earthLarge.jpeg'
-
-// const styles = {
-//   paperContainer: {
-//     backgroundImage: `url(${Image})`,
-//     backgroundRepeat: "no-repeat",
-//     backgroundPosition: "center center",
-//     backgroundSize: "cover",
-//     backgroundAttachment: "fixed",
-//     maxHeight: 'lg',
-//     maxWidth: 'lg',
-//   }
-// };
+import Image from '../images/earth.png';
 
 const useStyles = makeStyles((theme) => ({
-  
   heroContent: {
-    padding: theme.spacing(8, 0, 15),
-    // maxWidth: 'lg',
-    // maxHeight: 'lg',
+    padding: theme.spacing(8, 10, 15),
     backgroundImage: `url(${Image})`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center center",
-    // backgroungSize: "cover",
+    backgroungSize: "cover",
     backgroundAttachment: "fixed",
-    height: "100vh",
   },
   heroButtons: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5)
   },
+  spacing: {
+    backgroundColor: "black"
+  }
 }));
 
 export default function MainPage() {
   const classes = useStyles();
-  
+
   return (
-    <Paper>
+    <div>
         <div className={classes.heroContent}>
           <Container maxWidth="lg" >
             <Typography component="h1" variant="h2" align="center" color="white" gutterBottom>
               My Carbon Footprint
+            </Typography>
+            <Typography spacing= "10px">
+            What is carbon footprint?
+            Carbon footprint is the amount of carbon dioxide emissions directly or indirectly is generated from a person's activity. 
+            The average American produces 20 tons of CO2—three times higher than the world average. Here's your chance to put some of that carbon back in the ground.
             </Typography>
             <div className={classes.heroButtons}> 
               <Grid container spacing={2} justify="center">
@@ -73,9 +65,24 @@ export default function MainPage() {
               </div>
           </Container>
     </div> 
-          <footer>
-     <climate-clock />
-    </footer>
-    </Paper>
+    <div className={classes.spacing}>
+      <Typography component="h1" variant="h2" align="center" gutterBottom style= {{color: "white"}}>
+         Our Mission
+      </Typography>
+      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8}} >
+        The Climate Clock shows two numbers. The first, in red, is our deadline, the time we have left to take decisive action to keep warming under the 1.5°C threshold, 
+        a count down of how long it will take, at current rates of emissions, to burn through our carbon budget. 
+        The second number, in green, is the growing % of the world’s energy currently supplied from renewable sources. 
+        Our lifeline. The goal is to get our lifeline to 100% before our deadline reaches 0.
+      </Container>
+      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8}}>
+      Founded with the mission of helping us reach our lifeline before the dealine by showing that there is great potential for the collective actions of many individuals worldwide 
+      to reduce global warming by making changes in their daily and annual activities.
+      </Container>
+      </div>
+        <footer>
+           <climate-clock />
+        </footer>
+    </div>
   );
 }
