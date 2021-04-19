3270442e7ceaa5a92c6cdedc252a24eddd24a3f0
Melinda Cardenas <mc6377@nyu.edu>
2021-04-19 14:58:07 -0400

    add transportation quiz

--- src/components/footprint-quiz/transportation/transportationStep1.js
+++ src/components/footprint-quiz/transportation/transportationStep1.js
@@ -1,12 +1,25 @@
-import {Container, Typography} from '@material-ui/core/';
+import {Container, Typography, Card} from '@material-ui/core/';
 import PrimaryButton from '../../reusable/PrimaryButton'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function TransportationStep1(props){
-    const {currentStep, setCurrentStep, categories} = props
+    const {currentStep, setCurrentStep, categories, setModesOfTransport} = props
+    const [train, setTrain] = useState(false)
     const [bus, setBus] = useState(false)
-    const [fossilFueledCar, setFossilFueledCar] = useState(false)
+    const [car, setCar] = useState(false)
+    const [carSharing, setCarSharing] = useState(false)
+    //const [fossilFueledCar, setFossilFueledCar] = useState(false)
     const [electric, setElectric] = useState(false)
+    //const [hybridCar, setHybridCar] = useState(false)
+    const [motorbike, setMotorbike] = useState(false)
+    //const [shortDistanceBus, setShortDistanceBus] = useState(false)
+    //const [longDistanceBus, setLongDistanceBus] = useState(false)
+    const [boat, setBoat] = useState(false)
+    const [plane, setPlane] = useState(false)
+    
+    useEffect(()=>{
+        setModesOfTransport({train:train,bus:bus,car:car,carSharing:carSharing,electric:electric,motorbike:motorbike,boat:boat,plane:plane})
+    },[train,bus,car,carSharing,electric,motorbike,boat,plane])
 
     if(currentStep!=='TransportationStep1'){
         return null
@@ -14,12 +27,17 @@ function TransportationStep1(props){
     return(
         <Container maxWidth="sm">
             <Typography variant="h5" style={{color:'white'}}>
-                What form of transport did you take?
+                What mode(s) of transport did you take?
             </Typography>
+            <PrimaryButton selected={train} onClick={()=>setTrain(train=> !train)} text="Train"/>
             <PrimaryButton selected={bus} onClick={()=>setBus(bus=> !bus)} text="Bus"/>
-            <PrimaryButton selected={fossilFueledCar} onClick={()=>setFossilFueledCar(fossilFueledCar=> !fossilFueledCar)} text="Fossil Fueled Car"/>
+            {/* <PrimaryButton selected={fossilFueledCar} onClick={()=>setFossilFueledCar(fossilFueledCar=> !fossilFueledCar)} text="Fossil Fueled Car"/> */}
             <PrimaryButton selected={electric} onClick={()=>setElectric(electric=> !electric)} text="Electric Vehicle"/>
-
+            <PrimaryButton selected={car} onClick={()=>setCar(car=> !car)} text="Car"/>
+            <PrimaryButton selected={carSharing} onClick={()=>setCarSharing(carSharing=> !carSharing)} text="Car Sharing"/>
+            <PrimaryButton selected={motorbike} onClick={()=>setMotorbike(motorbike=> !motorbike)} text="Motorbike"/>
+            <PrimaryButton selected={boat} onClick={()=>setBoat(boat=> !boat)} text="Boat"/>
+            <PrimaryButton selected={plane} onClick={()=>setPlane(plane=> !plane)} text="Plane"/>
             {categories.food?<PrimaryButton onClick={()=>setCurrentStep('FoodStep2')} text="Go Back"/>:<PrimaryButton onClick={()=>setCurrentStep(1)} text="Go Back"/>}
             <PrimaryButton onClick={()=>setCurrentStep('TransportationStep2')} text="Next"/>
         </Container>
---
+++ src/components/footprint-quiz/transportation/transportationStep2.js
@@ -0,0 +1,52 @@
+import { useState,useEffect } from "react"
+import {Typography, TextField} from '@material-ui/core'
+import PrimaryButton from '../../reusable/PrimaryButton'
+
+export default function(props){
+    let {currentStep, setCurrentStep, modesOfTransport, setDistances} = props
+    const [trainDistance, setTrainDistance] = useState(0)
+    const [busDistance, setBusDistance] = useState(0)
+    const [carDistance, setCarDistance] = useState(0)
+    const [carSharingDistance, setCarSharingDistance] = useState(0)
+    const [electricDistance, setElectricDistance] = useState(0)
+    const [motorbikeDistance, setMotorbikeDistance] = useState(0)
+    const [boatDistance, setBoatDistance] = useState(0)
+    const [planeDistance, setPlaneDistance] = useState(0)
+
+    useEffect(()=>{
+        setDistances([
+            {type:'train', distance:trainDistance},
+            {type:'bus', distance:busDistance},
+            {type:'car', distance:carDistance},
+            {type:'carSharing', distance:carSharingDistance},
+            {type:'electricVehicle', distance:electricDistance},
+            {type:'motorbike', distance:motorbikeDistance},
+            {type:'boat', distance:boatDistance},
+            {type:'plane', distance:planeDistance}
+        ])
+    },[trainDistance,busDistance,carDistance,carSharingDistance,electricDistance,motorbikeDistance,boatDistance,planeDistance])
+
+    if(currentStep!=='TransportationStep2'){
+        return null
+    }
+
+    return(
+        <div>
+            <Typography variant="h5" style={{color:'white'}}>
+                How many kilometers did you travel in ...
+            </Typography>
+            {modesOfTransport.train && <TextField style={{margin:20}} id='trainDistance' label='Train' type="number" InputLabelProps={{shrink: true,}} variant="outlined" color="secondary" value={trainDistance} onChange={(e)=>setTrainDistance(e.target.value)}/>}
+            {modesOfTransport.bus && <TextField style={{margin:20}} id='busDistance' label='Bus' type="number" InputLabelProps={{shrink: true,}} variant="outlined" color="secondary" value={busDistance} onChange={(e)=>setBusDistance(e.target.value)}/>}
+            {modesOfTransport.car && <TextField style={{margin:20}} id='carDistance' label='Car' type="number" InputLabelProps={{shrink: true,}} variant="outlined" color="secondary" value={carDistance} onChange={(e)=>setCarDistance(e.target.value)}/>}
+            {modesOfTransport.carSharing && <TextField style={{margin:20}} id='carSharingDistance' label='Car Sharing' type="number" InputLabelProps={{shrink: true,}} variant="outlined" color="secondary" value={carSharingDistance} onChange={(e)=>setCarSharingDistance(e.target.value)}/>}
+            {modesOfTransport.electric && <TextField style={{margin:20}} id='electricDistance' label='Electric Vehicle' type="number" InputLabelProps={{shrink: true,}} variant="outlined" color="secondary" value={electricDistance} onChange={(e)=>setElectricDistance(e.target.value)}/>}
+            {modesOfTransport.motorbike && <TextField style={{margin:20}} id='motorbike' label='Motorbike' type="number" InputLabelProps={{shrink: true,}} variant="outlined" color="secondary" value={motorbikeDistance} onChange={(e)=>setMotorbikeDistance(e.target.value)}/>}
+            {modesOfTransport.boat && <TextField style={{margin:20}} id='boatDistance' label='Boat' type="number" InputLabelProps={{shrink: true,}} variant="outlined" color="secondary" value={boatDistance} onChange={(e)=>setBoatDistance(e.target.value)}/>}
+            {modesOfTransport.plane && <TextField style={{margin:20}} id='planeDistance' label='Plane' type="number" InputLabelProps={{shrink: true,}} variant="outlined" color="secondary" value={planeDistance} onChange={(e)=>setPlaneDistance(e.target.value)}/>}
+            <div>
+                <PrimaryButton onClick={()=>setCurrentStep('TransportationStep1')} text="Previous"/>
+                <PrimaryButton type="submit" text="Submit"/>
+            </div>
+        </div>
+    )
+}
