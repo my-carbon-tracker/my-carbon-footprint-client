ae116c2d2b80b878072354e98372bf7d5cb35dc2
LuisToro-v <Vidals824@yahoo.com>
2021-04-22 10:45:57 -0400

    Style main (#18)
    * Changed background, style buttons, and added a background to Container
    
    * Changed backgorund image
    
    * Adjusted padding and alignment
    
    * Edit text
    
    * Made text changes

--- src/components/main.js
+++ src/components/main.js
@@ -4,23 +4,39 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Image from '../images/earth.png';
+// import Image from '../images/earth.png';
+import Logo from '../images/Logo.png';
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
-    padding: theme.spacing(8, 10, 30),
-    backgroundImage: `url(${Image})`,
+    padding: theme.spacing(8, 8, 16),
+    backgroundImage: `url(https://res.cloudinary.com/dd6dpafkm/image/upload/v1618611186/ice_signin_gheuo2.jpg)`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center center",
     backgroungSize: "cover",
     backgroundAttachment: "fixed",
+    background: 'linear-gradient(#DFB593, #DF7B7D 50%)',
+    color:'white',
   },
   heroButtons: {
     marginTop: theme.spacing(5),
-    marginBottom: theme.spacing(5)
+    marginBottom: theme.spacing(5),
   },
   spacing: {
     backgroundColor: "black"
+  },
+  btn: {
+    background: 'linear-gradient(#DFB593, #DF7B7D 50%)',
+    borderRadius: 20,
+    border: 5,
+    color: 'white',
+    height:'7.7vh',
+   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  textBox: {
+    backgroundColor:"rgba(196,196,196,.3)",
+    borderRadius: 20,
+    padding: '10px 10px 15px',
   }
 }));
 
@@ -30,34 +46,35 @@ export default function MainPage() {
   return (
     <div>
         <div className={classes.heroContent}>
-          <Container maxWidth="lg" >
-            <Typography component="h1" variant="h2" align="center" color="white" gutterBottom>
+          <Container maxWidth="lg" className = {classes.textBox}>
+            <Typography component="h1" variant="h2" align="center" gutterBottom>
               My Carbon Footprint
             </Typography>
-            <Typography spacing= "10px">
-            What is carbon footprint?
-            Carbon footprint is the amount of carbon dioxide emissions directly or indirectly is generated from a person's activity. 
-            The average American produces 20 tons of CO2—three times higher than the world average. Here's your chance to put some of that carbon back in the ground.
+            <Typography spacing= "10px" >
+            <strong>What is A Carbon Footprint? </strong>
+            <div></div>
+            A carbon footprint is a form of measurement that expresses the amount of carbon dioxide generated through the burring of fossil fuels as the result of human activities.
+            The average American produces 20 tons of CO2, three times higher than the World's average. Here's your chance to put some of that carbon back in the ground.
             </Typography>
             <div className={classes.heroButtons}> 
               <Grid container spacing={2} justify="center">
                 <Grid item>
-                  <Button variant="contained" color="primary" href="/quiz">
+                  <Button variant="contained" className = {classes.btn} href="/quiz">
                     In Depth Analysis
                   </Button>
                   </Grid>
                 <Grid item>
-                  <Button variant="contained" color="primary" href="/carbon-estimation">
+                  <Button variant="contained" className = {classes.btn} href="/carbon-estimation">
                     Assestment
                   </Button>
                 </Grid>
                 <Grid item>
-                  <Button variant="contained" color="primary" href="/login">
+                  <Button variant="contained" className = {classes.btn} href="/login">
                     Login
                   </Button>
                   </Grid>
                   <Grid item>
-                  <Button variant="contained" color="primary" href="/register">
+                  <Button variant="contained" className = {classes.btn} href="/register">
                     Signup 
                   </Button>
                   </Grid>
@@ -65,19 +82,20 @@ export default function MainPage() {
               </div>
           </Container>
     </div> 
-    <div className={classes.spacing}>
-      <Typography component="h1" variant="h2" align="center" gutterBottom style= {{color: "white"}}>
+    <div className={classes.spacing} align="center">
+      <Typography component="h1" variant="h2"  gutterBottom style= {{color: "white"}}>
          Our Mission
       </Typography>
-      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8}} >
+      <img src={Logo} height="10%" width="10%"/>
+      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8, textAlign:'left'}} >
         The Climate Clock shows two numbers. The first, in red, is our deadline, the time we have left to take decisive action to keep warming under the 1.5°C threshold, 
-        a count down of how long it will take, at current rates of emissions, to burn through our carbon budget. 
-        The second number, in green, is the growing % of the world’s energy currently supplied from renewable sources. 
-        Our lifeline. The goal is to get our lifeline to 100% before our deadline reaches 0.
+        a count down of how long it will take, at our current rates of emissions, to burn through our carbon budget. 
+        The second number, in green, is the growing percentage of the world’s energy currently supplied by renewable sources,
+        our lifeline. The goal is to get our lifeline to 100% before our deadline reaches 0.
       </Container>
-      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8}}>
-      Founded with the mission of helping us reach our lifeline before the dealine by showing that there is great potential for the collective actions of many individuals worldwide 
-      to reduce global warming by making changes in their daily and annual activities.
+      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8,textAlign:'left',}}>
+     Founded by our mission of helping us reach our lifeline before the deadline by demonstrating that there exist great potential for 
+     the collective actions of individuals worldwide to make an impact and reduce global warming by making changes in our daily life
       </Container>
       </div>
         <footer>
