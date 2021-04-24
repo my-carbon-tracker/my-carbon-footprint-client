4965c334309a9165e62e054786cc9e91f1ccf770
Luis <vidals824@yahoo.com>
2021-04-23 23:36:32 -0400

    Changed font size to 16

--- src/components/main.js
+++ src/components/main.js
@@ -6,10 +6,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Team from "./team";
 import Logo from "../images/pinkLogo.png";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
-    padding: theme.spacing(16, 30, 16),
+    padding: theme.spacing(13, 30, 13),
     backgroundImage: `url(https://res.cloudinary.com/dd6dpafkm/image/upload/v1618611186/ice_signin_gheuo2.jpg)`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center center",
@@ -37,6 +38,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const theme = createMuiTheme({
+  typography: {
+    fontSize: 16,
+  },
+});
+
 export default function MainPage() {
   const classes = useStyles();
 
@@ -47,18 +54,20 @@ export default function MainPage() {
           <Typography component="h1" variant="h2" align="center" gutterBottom>
             My Carbon Footprint
           </Typography>
-          <Typography spacing="10px">
-            <strong>What is A Carbon Footprint? </strong>
-            <div></div>
-            <span>
-              A carbon footprint is a form of measurement that expresses the
-              amount of carbon dioxide generated through the burring of fossil
-              fuels as the result of human activities. The average American
-              produces 20 tons of CO2, three times higher than the World's
-              average. Here's your chance to put some of that carbon back in the
-              ground.
-            </span>
-          </Typography>
+          <ThemeProvider theme={theme}>
+            <Typography spacing="10px">
+              <strong>What is A Carbon Footprint? </strong>
+              <div></div>
+              <span>
+                A carbon footprint is a form of measurement that expresses the
+                amount of carbon dioxide generated through the burring of fossil
+                fuels as the result of human activities. The average American
+                produces 20 tons of CO2, three times higher than the World's
+                average. Here's your chance to put some of that carbon back in
+                the ground.
+              </span>
+            </Typography>
+          </ThemeProvider>
           <div className={classes.heroButtons}>
             <Grid container spacing={3} justify="center">
               <Grid item>
@@ -115,43 +124,45 @@ export default function MainPage() {
           Our Mission
         </Typography>
         <img src={Logo} height="10%" width="10%" />
-        <div
-          style={{
-            padding: "3% 20% 5%",
-            borderColor: "white",
-          }}
-        >
-          <Container
+        <ThemeProvider theme={theme}>
+          <Typography
             style={{
-              color: "white",
-              wordSpacing: 3,
-              marginTop: 3,
-              textAlign: "left",
+              padding: "3% 20% 5%",
+              borderColor: "white",
             }}
           >
-            The Climate Clock shows two numbers. The first, in red, is our
-            deadline, the time we have left to take decisive action to keep
-            warming under the 1.5°C threshold, a count down of how long it will
-            take, at our current rates of emissions, to burn through our carbon
-            budget. The second number, in green, is the growing percentage of
-            the world’s energy currently supplied by renewable sources, our
-            lifeline. The goal is to get our lifeline to 100% before our
-            deadline reaches 0.
-          </Container>
-          <Container
-            style={{
-              color: "white",
-              wordSpacing: 3,
-              marginTop: 8,
-              textAlign: "left",
-            }}
-          >
-            Founded by our mission of helping us reach our lifeline before the
-            deadline by demonstrating that there exist great potential for the
-            collective actions of individuals worldwide to make an impact and
-            reduce global warming by making changes in our daily life
-          </Container>
-        </div>
+            <Container
+              style={{
+                color: "white",
+                wordSpacing: 3,
+                marginTop: 3,
+                textAlign: "left",
+              }}
+            >
+              The Climate Clock shows two numbers. The first, in red, is our
+              deadline, the time we have left to take decisive action to keep
+              warming under the 1.5°C threshold, a count down of how long it
+              will take, at our current rates of emissions, to burn through our
+              carbon budget. The second number, in green, is the growing
+              percentage of the world’s energy currently supplied by renewable
+              sources, our lifeline. The goal is to get our lifeline to 100%
+              before our deadline reaches 0.
+            </Container>
+            <Container
+              style={{
+                color: "white",
+                wordSpacing: 3,
+                marginTop: 8,
+                textAlign: "left",
+              }}
+            >
+              Founded by our mission of helping us reach our lifeline before the
+              deadline by demonstrating that there exist great potential for the
+              collective actions of individuals worldwide to make an impact and
+              reduce global warming by making changes in our daily life
+            </Container>
+          </Typography>
+        </ThemeProvider>
         <Container
           style={{
             color: "white",
