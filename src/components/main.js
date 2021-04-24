b03ea3732f043b0f1c1b99a1d8aecd362cd22fde
LuisToro-v <Vidals824@yahoo.com>
2021-04-24 10:57:42 -0400

    Edit main (#22)
    * Adjusted padding each of each section, made text area narrower and change button color
    
    * Change padding of Team banner and change text to Meet the Team
    
    * Changed font size to 16

--- src/components/main.js
+++ src/components/main.js
@@ -1,111 +1,183 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Team from './team';
-import Logo from '../images/darkLogo.png';
-import Footer from './footer';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Team from "./team";
+import Logo from "../images/darkLogo.png";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Footer from "./footer";
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
-    padding: theme.spacing(8, 8, 16),
+    padding: theme.spacing(13, 30, 13),
     backgroundImage: `url(https://res.cloudinary.com/dd6dpafkm/image/upload/v1618611186/ice_signin_gheuo2.jpg)`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center center",
     backgroungSize: "cover",
     backgroundAttachment: "fixed",
-    background: 'linear-gradient(#DFB593, #DF7B7D 50%)',
-    color:'white',
+    background: "linear-gradient(#DFB593, #DF7B7D 50%)",
+    color: "white",
   },
   heroButtons: {
     marginTop: theme.spacing(5),
     marginBottom: theme.spacing(5),
   },
   spacing: {
-    backgroundColor: "black"
+    padding: theme.spacing(12, 5, 1),
+    backgroundColor: "black",
   },
   btn: {
-    background: 'linear-gradient(#DFB593, #DF7B7D 50%)',
-    borderRadius: 20,
-    border: 5,
-    color: 'white',
-    height:'7.7vh',
-   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    opacity: 0.75,
+    height: "7.0vh",
   },
   textBox: {
-    backgroundColor:"rgba(196,196,196,.3)",
+    backgroundColor: "rgba(196,196,196,.3)",
     borderRadius: 20,
-    padding: '10px 10px 15px',
-  }
+    padding: theme.spacing(10, 10, 10),
+  },
 }));
+
+const theme = createMuiTheme({
+  typography: {
+    fontSize: 16,
+  },
+});
 
 export default function MainPage() {
   const classes = useStyles();
 
   return (
     <div>
-        <div className={classes.heroContent}>
-          <Container maxWidth="lg" className = {classes.textBox}>
-            <Typography component="h1" variant="h2" align="center" gutterBottom>
-              My Carbon Footprint
+      <div className={classes.heroContent}>
+        <Container maxWidth="lg" className={classes.textBox}>
+          <Typography component="h1" variant="h2" align="center" gutterBottom>
+            My Carbon Footprint
+          </Typography>
+          <ThemeProvider theme={theme}>
+            <Typography spacing="10px">
+              <strong>What is A Carbon Footprint? </strong>
+              <div></div>
+              <span>
+                A carbon footprint is a form of measurement that expresses the
+                amount of carbon dioxide generated through the burring of fossil
+                fuels as the result of human activities. The average American
+                produces 20 tons of CO2, three times higher than the World's
+                average. Here's your chance to put some of that carbon back in
+                the ground.
+              </span>
             </Typography>
-            <Typography spacing= "10px" >
-            <strong>What is A Carbon Footprint? </strong>
-            <div></div>
-            A carbon footprint is a form of measurement that expresses the amount of carbon dioxide generated through the burring of fossil fuels as the result of human activities.
-            The average American produces 20 tons of CO2, three times higher than the World's average. Here's your chance to put some of that carbon back in the ground.
-            </Typography>
-            <div className={classes.heroButtons}> 
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" className = {classes.btn} href="/quiz">
-                    In Depth Analysis
-                  </Button>
-                  </Grid>
-                <Grid item>
-                  <Button variant="contained" className = {classes.btn} href="/carbon-estimation">
-                    General Assestment
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="contained" className = {classes.btn} href="/login">
-                    Login
-                  </Button>
-                  </Grid>
-                  <Grid item>
-                  <Button variant="contained" className = {classes.btn} href="/register">
-                    Signup 
-                  </Button>
-                  </Grid>
+          </ThemeProvider>
+          <div className={classes.heroButtons}>
+            <Grid container spacing={3} justify="center">
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.btn}
+                  href="/quiz"
+                >
+                  In Depth Analysis
+                </Button>
               </Grid>
-              </div>
-          </Container>
-    </div> 
-    <div className={classes.spacing} align="center">
-      <Typography component="h1" variant="h2"  gutterBottom style= {{color: "white"}}>
-         Our Mission
-      </Typography>
-      <img src={Logo} height="10%" width="10%"/>
-
-      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8, textAlign:'left'}} >
-        The Climate Clock shows two numbers. The first, in red, is our deadline, the time we have left to take decisive action to keep warming under the 1.5°C threshold, 
-        a count down of how long it will take, at our current rates of emissions, to burn through our carbon budget. 
-        The second number, in green, is the growing percentage of the world’s energy currently supplied by renewable sources,
-        our lifeline. The goal is to get our lifeline to 100% before our deadline reaches 0.
-      </Container>
-      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8,textAlign:'left',}}>
-     Founded by our mission of helping us reach our lifeline before the deadline by demonstrating that there exist great potential for 
-     the collective actions of individuals worldwide to make an impact and reduce global warming by making changes in our daily life
-      </Container>
-
-      <Container style= {{color:"white", wordSpacing: 3, marginTop: 8,textAlign:'left',}}>
-        <Team />
-      </Container>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.btn}
+                  href="/carbon-estimation"
+                >
+                  General Assestment
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.btn}
+                  href="/login"
+                >
+                  Login
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.btn}
+                  href="/register"
+                >
+                  Signup
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
       </div>
-           <climate-clock />
-           <Footer />
+      <div className={classes.spacing} align="center">
+        <Typography
+          component="h1"
+          variant="h2"
+          gutterBottom
+          style={{ color: "white" }}
+        >
+          Our Mission
+        </Typography>
+        <img src={Logo} height="10%" width="10%" />
+        <ThemeProvider theme={theme}>
+          <Typography
+            style={{
+              padding: "3% 20% 5%",
+              borderColor: "white",
+            }}
+          >
+            <Container
+              style={{
+                color: "white",
+                wordSpacing: 3,
+                marginTop: 3,
+                textAlign: "left",
+              }}
+            >
+              The Climate Clock shows two numbers. The first, in red, is our
+              deadline, the time we have left to take decisive action to keep
+              warming under the 1.5°C threshold, a count down of how long it
+              will take, at our current rates of emissions, to burn through our
+              carbon budget. The second number, in green, is the growing
+              percentage of the world’s energy currently supplied by renewable
+              sources, our lifeline. The goal is to get our lifeline to 100%
+              before our deadline reaches 0.
+            </Container>
+            <Container
+              style={{
+                color: "white",
+                wordSpacing: 3,
+                marginTop: 8,
+                textAlign: "left",
+              }}
+            >
+              Founded by our mission of helping us reach our lifeline before the
+              deadline by demonstrating that there exist great potential for the
+              collective actions of individuals worldwide to make an impact and
+              reduce global warming by making changes in our daily life
+            </Container>
+          </Typography>
+        </ThemeProvider>
+        <Container
+          style={{
+            color: "white",
+            wordSpacing: 3,
+            marginTop: 8,
+            textAlign: "left",
+          }}
+        >
+          <Team />
+        </Container>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/components/team.js
+++ src/components/team.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -16,7 +16,8 @@ const useStyles = makeStyles((theme) => ({
   },
   heroContent: {
     backgroundColor: "#FED7D9",
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(5, 10, 5),
+    margin: theme.spacing(0, 18, 0),
   },
   heroButtons: {
     marginTop: theme.spacing(4),
@@ -27,12 +28,12 @@ const useStyles = makeStyles((theme) => ({
   },
   card: {
     backgroundColor: "#FED7D9",
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   cardMedia: {
-    paddingTop: '90%', // 16:9
+    paddingTop: "90%", // 16:9
   },
   cardContent: {
     flexGrow: 1,
@@ -45,115 +46,188 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Team() {
   const classes = useStyles();
-  
+
   return (
     <React.Fragment>
       <CssBaseline />
       <main>
         <div className={classes.heroContent}>
-          <Container maxWidth="sm" >
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                Our Amazing Team
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Meet the Team
             </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-            “Saving the planet one code at a time” - Climate Developers
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              “Saving the planet one code at a time” - Climate Developers
             </Typography>
           </Container>
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
- 
           <Grid container spacing={3}>
+            <Grid item xs={12} sm={4} md={3}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://media-exp1.licdn.com/dms/image/C4D03AQFWxprSI6xqoA/profile-displayphoto-shrink_800_800/0/1607478405412?e=1624492800&v=beta&t=TL5GoJznT5YhPtvJzQOPI8cYa5waWwVCte-y16yCmdM"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Jaylene Salas
+                  </Typography>
+                  <Typography>Product Manager, Software Engineer</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    <p>
+                      <a href="https://github.com/Jaylene-s">
+                        <img
+                          src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png"
+                          width="30%"
+                          height="30%"
+                        />
+                      </a>
+                    </p>
+                    <p>
+                      {" "}
+                      <a href="https://www.linkedin.com/in/jaylene-salas/">
+                        <img
+                          src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_LinkedIn-128.png"
+                          width="30%"
+                          height="30%"
+                        />
+                      </a>
+                    </p>
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
 
-              <Grid item  xs={12} sm={4} md={3}>
-                <Card className={classes.card}>
-                  <CardMedia className={classes.cardMedia}
-                    image="https://media-exp1.licdn.com/dms/image/C4D03AQFWxprSI6xqoA/profile-displayphoto-shrink_800_800/0/1607478405412?e=1624492800&v=beta&t=TL5GoJznT5YhPtvJzQOPI8cYa5waWwVCte-y16yCmdM" />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Jaylene Salas
-                    </Typography>
-                    <Typography>
-                      Product Manager, Software Engineer
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                    <p><a href="https://github.com/Jaylene-s"><img src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png" width="30%" height="30%"/></a></p>
-                      <p> <a href="https://www.linkedin.com/in/jaylene-salas/" ><img src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_LinkedIn-128.png" width="30%" height="30%"/></a></p>
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://media-exp1.licdn.com/dms/image/C4D03AQHSmA20vn57Ww/profile-displayphoto-shrink_800_800/0/1618945816879?e=1624492800&v=beta&t=7R1v9UyuLB3lih9UiUqTjHRJ8oeG39jR4xrvZhnAL0A"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Ebony Brown
+                  </Typography>
+                  <Typography>Scrum Master, Software Engineer</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    <p>
+                      <a href="https://github.com/e-mbrown">
+                        <img
+                          src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png"
+                          width="30%"
+                          height="30%"
+                        />
+                      </a>
+                    </p>
+                    <p>
+                      {" "}
+                      <a href="https://www.linkedin.com/in/ebony-brown-884b331ab/">
+                        <img
+                          src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_LinkedIn-128.png"
+                          width="30%"
+                          height="30%"
+                        />
+                      </a>
+                    </p>
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
 
-              <Grid item  xs={12} sm={4} md={3}>
-                <Card className={classes.card}>
-                  <CardMedia className={classes.cardMedia}
-                    image="https://media-exp1.licdn.com/dms/image/C4D03AQHSmA20vn57Ww/profile-displayphoto-shrink_800_800/0/1618945816879?e=1624492800&v=beta&t=7R1v9UyuLB3lih9UiUqTjHRJ8oeG39jR4xrvZhnAL0A" />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Ebony Brown
-                    </Typography>
-                    <Typography>
-                      Scrum Master, Software Engineer
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                    <p><a href="https://github.com/e-mbrown"><img src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png" width="30%" height="30%"/></a></p>
-                      <p> <a href="https://www.linkedin.com/in/ebony-brown-884b331ab/" ><img src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_LinkedIn-128.png" width="30%" height="30%"/></a></p>
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://media-exp1.licdn.com/dms/image/C4D03AQGmoNPWJh9rBA/profile-displayphoto-shrink_800_800/0/1605232883544?e=1624492800&v=beta&t=R5TmqxSPl02jD8zhF-Ir0wDM3ut9wbuS2TyrX-xnC-c"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Melinda Cardenas
+                  </Typography>
+                  <Typography>Software Engineer</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    <p>
+                      <a href="https://github.com/MelindaCardenas">
+                        <img
+                          src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png"
+                          width="30%"
+                          height="30%"
+                        />
+                      </a>
+                    </p>
+                    <p>
+                      {" "}
+                      <a href="https://www.linkedin.com/in/melinda-cardenas/">
+                        <img
+                          src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_LinkedIn-128.png"
+                          width="30%"
+                          height="30%"
+                        />
+                      </a>
+                    </p>
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
 
-              <Grid item  xs={12} sm={4} md={3}>
-                <Card className={classes.card}>
-                  <CardMedia className={classes.cardMedia}
-                    image="https://media-exp1.licdn.com/dms/image/C4D03AQGmoNPWJh9rBA/profile-displayphoto-shrink_800_800/0/1605232883544?e=1624492800&v=beta&t=R5TmqxSPl02jD8zhF-Ir0wDM3ut9wbuS2TyrX-xnC-c" />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Melinda Cardenas
-                    </Typography>
-                    <Typography>
-                      Software Engineer
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                    <p><a href="https://github.com/MelindaCardenas"><img src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png" width="30%" height="30%"/></a></p>
-                      <p> <a href="https://www.linkedin.com/in/melinda-cardenas/" ><img src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_LinkedIn-128.png" width="30%" height="30%"/></a></p>
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-
-              <Grid item  xs={12} sm={4} md={3}>
-                <Card className={classes.card}>
-                  <CardMedia className={classes.cardMedia}
-                    image="https://media-exp1.licdn.com/dms/image/C4D03AQEC252ACEElAQ/profile-displayphoto-shrink_800_800/0/1549417066822?e=1624492800&v=beta&t=0SApiv2k-hWujD-u-C1HTtj98BBPAO3BtapebcA948Y" />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Luis Toro
-                    </Typography>
-                    <Typography>
-                      Software Engineer
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                        <p><a href="https://github.com/LuisToro-v"><img src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png" width="30%" height="30%"/></a></p>
-                      <p> <a href="https://www.linkedin.com/in/luis-toro-bbb464159/" ><img src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_LinkedIn-128.png" width="30%" height="30%"/></a></p>
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-
-              
-
+            <Grid item xs={12} sm={4} md={3}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://media-exp1.licdn.com/dms/image/C4D03AQEC252ACEElAQ/profile-displayphoto-shrink_800_800/0/1549417066822?e=1624492800&v=beta&t=0SApiv2k-hWujD-u-C1HTtj98BBPAO3BtapebcA948Y"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Luis Toro
+                  </Typography>
+                  <Typography>Software Engineer</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    <p>
+                      <a href="https://github.com/LuisToro-v">
+                        <img
+                          src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png"
+                          width="30%"
+                          height="30%"
+                        />
+                      </a>
+                    </p>
+                    <p>
+                      {" "}
+                      <a href="https://www.linkedin.com/in/luis-toro-bbb464159/">
+                        <img
+                          src="https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_LinkedIn-128.png"
+                          width="30%"
+                          height="30%"
+                        />
+                      </a>
+                    </p>
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
           </Grid>
         </Container>
       </main>
-
     </React.Fragment>
   );
 }
