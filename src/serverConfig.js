d7a3808b77238a0685c8adc9b39ffb5e5bd65dda
Melinda Cardenas <43328787+MelindaCardenas@users.noreply.github.com>
2021-04-25 12:50:15 -0400

    delete extra '/' in server url, testing again (#27)
    * fix bug in pages rendering
    
    * cleaning up food quiz + adding UI
    
    * change secondary color
    
    * comment out code
    
    * starting transportation quiz
    
    * add transportation quiz
    
    * add fetch for transport quiz
    
    * using decimals instead of ints for total emissions
    
    * add more styling for quiz
    
    * changed units to meters
    
    * changing transportation quiz to use miles
    
    * add styling for estimation quiz
    
    * add styling for top of home page
    
    * uncomment out button
    
    * add basic styling for table titles + spacing
    
    * fix tukey showing up bug
    
    * add fetch to adjust total emissions
    
    * password hidden bug fix
    
    * delete line
    
    * remove climate clock from home
    
    * add tableau to package json
    
    * remove space
    
    * add fetch call to add to users total emissions
    
    * add paper translucent background to quiz
    
    * change styling of primary button to dark navy blue
    
    * changing styling for paper + tables in home
    
    * use mui for pledges table
    
    * add confetti to package json
    
    * switch places of pledges and leader board
    
    * make leaderboard white translucent with slight border
    
    * adding border radius to pledges table
    
    * change background color to more muted pink
    
    * add server config file, testing if works
    
    * delete extra '/' in server string. testing again

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,7 +1,7 @@
 const getServerURL = () => {
     if (process.env.NODE_ENV === 'production') {
       //return 'https://server.keep.id';
-      return 'https://my-carbon-footprint.herokuapp.com/'
+      return 'https://my-carbon-footprint.herokuapp.com'
     }
     return 'http://localhost:3000';
   };
