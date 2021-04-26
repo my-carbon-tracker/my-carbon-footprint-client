0cb3f30b36408e364be169326c91af3de23ed218
Melinda Cardenas <mc6377@nyu.edu>
2021-04-26 14:18:42 -0400

    remove unnecessary line

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,6 +1,5 @@
 const getServerURL = () => {
     if (process.env.NODE_ENV === 'production') {
-      //return 'https://server.keep.id';
       return 'https://my-carbon-footprint.herokuapp.com'
     }
     return 'http://localhost:3000';
